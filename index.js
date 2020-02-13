00284c386c6b79577f9374e404b83b07c32ed64d
Somalika <somalikaboeuy138@gmail.com>
2020-02-13 14:42:21 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const {Builder, By, Key, util} = require("selenium-webdriver");
+async function example() {
+  let driver = await new Builder().forBrowser("firefox").build();
+  await driver.get()
+}
